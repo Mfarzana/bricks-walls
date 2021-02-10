91d7990a5f0c430f258467aa2517ccdf4a23307c
Mfarzana <mfarzana1133@gmail.com>
2021-02-10 17:10:48 +0900

    commit all changes

--- bricks.js
+++ bricks.js
@@ -1,29 +1,31 @@
  // This code for make square block when DOM loaded
 $('document').ready(function(){
-     event.preventDefault(); //stops refreshing       
 
     for (var i = 1; i <30; i++) {
         var div = document.createElement("div");
         $(div).addClass( "drag" );
-        $(div).attr('id', 'b'+i);             
        //pickColor:randomly assign a color for the square's background
         div.style.background = pickColor();
         // also set the width and height of the squares in the css
         div.style.left=pickPosition();
         div.style.top=pickPosition();
-
-        $("#bricks").append(div); 
-        $(".drag").draggable();
+        $("#bricks").append(div);         
         console.log(div);
     }
+    $(".drag").draggable(
+        {
+            containment: $('#walls'),
+        }
+    );
 });
 
+/*
 $( "#walls" ).droppable({
     drop: function( event, ui ) {
         $( this )
         .addClass( "ui-state-highlight" )        
     }
-});
+});*/
 // pick random color
 function pickColor(){ 
     var colorArray = ["red", "green", "yellow", "blue","orange","black"];
